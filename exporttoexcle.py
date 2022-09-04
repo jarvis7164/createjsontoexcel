eccabcf89503c324aa0ba6ee2aadb6c11052ec10
jarvis7164 <309194437@qq.com>
2022-09-04 22:10:32 +0800

    重写exporttoexcle

--- exporttoexcle.py
+++ exporttoexcle.py
@@ -6,15 +6,27 @@
 # @Software: PyCharm
 import openpyxl as op
 
+class write_excel_xlsx:
 
-# 导出数据到excel的类
-class exporttoexcle:
-    def exportoexcle(self,a, b,c):
-        wb = op.Workbook()  # 创建工作簿对象
-        ws = wb['Sheet']  # 创建sheet页
-        ws.cell(row=1, column=1).value = a  # 第一行第一列赋值a
-        ws.cell(row=1, column=2).value = b
-        wb.save(c)  # 保存excle
+    def write_excel_xlsx(path, sheet_name, value):
+        index = len(value)
+        workbook = op.Workbook()
+        sheet = workbook.active
+        sheet.title = sheet_name
+        for i in range(0, index):
+            for j in range(0, len(value[i])):
+                sheet.cell(row=i+1, column=j+1, value=str(value[i][j]))
+        workbook.save(path)
+        print("xlsx格式表格写入数据成功！")
 
-a = exporttoexcle()
-a.exportoexcle(1,2,'abc.xlsx')
+
+    def read_excel_xlsx(path, sheet_name):
+        workbook = op.load_workbook(path)
+        # sheet = wb.get_sheet_by_name(sheet_name)这种方式已经弃用，不建议使用
+        sheet = workbook[sheet_name]
+        for row in sheet.rows:
+            for cell in row:
+                print(cell.value, "\t", end="")
+            print()
+
+
